576e12a15a83c7658302d38e0f85db9c38a1c1a9
律-Ritsu <me@ritsu-me.com>
2023-05-08 14:53:13 +0000

    [update] 拡張子の修正

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createBot, Intents, startBot, CreateSlashApplicationCommand, InteractionResponseTypes } from "./deps.js"
-import { Secret } from "./secret.js"
+import { createBot, Intents, startBot, CreateSlashApplicationCommand, InteractionResponseTypes } from "./deps.ts"
+import { Secret } from "./secret.ts"
 
 const bot = createBot({
     token: Secret.DISCORD_TOKEN,
