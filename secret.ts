0b8a517e45c563607e0c2a5fc2d3b55dcb01faca
律-Ritsu <me@ritsu-me.com>
2023-05-09 15:35:54 +0000

    [edit] env loading system

--- secret.ts
+++ secret.ts
@@ -1,13 +1,14 @@
 import { dotenv } from "./deps.ts"
-import { Deno } from "./main.ts"
 
 
 await dotenv.config({
     export: true,
     path: "./.env.local",
 });
-  
+
+const config = Deno.env.toObject();
+
 export const Secret = {
-    DISCORD_TOKEN: Deno.env.get("DISCORD_TOKEN"),
-    GUILD_ID: Deno.env.get("GUILD_ID")
+    DISCORD_TOKEN: config["DISCORD_TOKEN"],
+    GUILD_ID: config["GUILD_ID"]
 };
