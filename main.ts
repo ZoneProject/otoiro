9d374a7fb753eb97656efc7a8f921084746e1857
ritsu-me <me@ritsu-me.com>
2023-05-29 02:47:12 +0900

    Changed to discord.js

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Message, Client, Events, GatewayIntentBits } from 'discord.js'
+import { Message, Client, Events, GatewayIntentBits, EmbedBuilder } from 'discord.js'
 
 import dotenv from 'dotenv'
 dotenv.config();
@@ -7,8 +7,8 @@ const client:Client = new Client({
   intents: Object.values(GatewayIntentBits).filter((v): v is number => typeof v === 'number'),
 });
 
-client.once(Events.ClientReady, (c: Client) => {
-    console.log(`Ready! Logged in as ${c.user?.tag}`);
+client.once(Events.ClientReady, (client: Client) => {
+    console.log(`Ready! Logged in as ${client.user?.tag}`);
 });
 
 client.on(Events.MessageCreate, async (message: Message) => {
@@ -16,9 +16,30 @@ client.on(Events.MessageCreate, async (message: Message) => {
         return;
     }
 
-    if (message.content.startsWith('!ping')) {
-        message.reply('Pong!');
+    if (message.content.startsWith('!')) {
+        message.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setTitle("Sorry...")
+                .setDescription("The traditional message commands has done it's service.\nPlease use 'Slash Commands' --the Discord's official command functions-- instead.\nThank you for your cooperation.")
+                .setColor("#2f3136")
+            ]
+        })
+        return;
     }
 });
+
+client.on(Events.InteractionCreate, async (interaction:/*ここの型定義どうすればええんや*/) => {
+    if (interaction.channel?.type == "DM"){
+        interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setTitle("Sorry...")
+                .setDescription("you can't use commands at DM.")
+                .setColor("#2f3136")
+            ]
+        })
+    }
+})
 
 client.login(process.env.DISCORD_TOKEN);
