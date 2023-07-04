26a27ce2f259ba514819c692b5a223660ba9d48f
ritsu-me <me@ritsu-me.com>
2023-07-04 20:23:29 +0900

    aaa

--- main.ts
+++ main.ts
@@ -1,45 +1,58 @@
-import { Message, Client, Events, GatewayIntentBits, EmbedBuilder } from 'discord.js'
-
-import dotenv from 'dotenv'
+const {
+  Message,
+  Client,
+  Events,
+  GatewayIntentBits,
+  EmbedBuilder,
+  BaseInteraction,
+} = require("discord.js");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const client:Client = new Client({
-  intents: Object.values(GatewayIntentBits).filter((v): v is number => typeof v === 'number'),
+const client: typeof Client = new Client({
+  intents: Object.values(GatewayIntentBits).filter(
+    (v): v is number => typeof v === "number"
+  ),
 });
 
-client.once(Events.ClientReady, (client: Client) => {
-    console.log(`Ready! Logged in as ${client.user?.tag}`);
+client.once(Events.ClientReady, (client: typeof Client) => {
+  console.log(`Ready! Logged in as ${client.user?.tag}`);
 });
 
-client.on(Events.MessageCreate, async (message: Message) => {
-    if (message.author.bot) {
-        return;
-    }
+client.on(Events.MessageCreate, async (message: typeof Message) => {
+  if (message.author.bot) {
+    return;
+  }
 
-    if (message.content.startsWith('!')) {
-        message.reply({
-            embeds: [
-                new EmbedBuilder()
-                .setTitle("Sorry...")
-                .setDescription("The traditional message commands has done it's service.\nPlease use 'Slash Commands' --the Discord's official command functions-- instead.\nThank you for your cooperation.")
-                .setColor("#2f3136")
-            ]
-        })
-        return;
-    }
+  if (message.content.startsWith("!")) {
+    await message.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("Sorry...")
+          .setDescription(
+            "The traditional message commands has done it's service.\nPlease use 'Slash Commands' --the Discord's official command functions-- instead.\nThank you for your cooperation."
+          )
+          .setColor("#2f3136"),
+      ],
+    });
+    return;
+  }
 });
 
-client.on(Events.InteractionCreate, async (interaction:/*ここの型定義どうすればええんや*/) => {
-    if (interaction.channel?.type == "DM"){
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                .setTitle("Sorry...")
-                .setDescription("you can't use commands at DM.")
-                .setColor("#2f3136")
-            ]
-        })
+client.on(
+  Events.InteractionCreate,
+  async (interaction: typeof BaseInteraction) => {
+    if (interaction.channel?.type == "DM") {
+      await interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("Sorry...")
+            .setDescription("you can't use commands at DM.")
+            .setColor("#2f3136"),
+        ],
+      });
     }
-})
+  }
+);
 
 client.login(process.env.DISCORD_TOKEN);
