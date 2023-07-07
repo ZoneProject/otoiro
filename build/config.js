6b47e6f3829f4df7dd7b65d34ca469086630ef05
ritsu-me <me@ritsu-me.com>
2023-07-07 19:57:30 +0900

    aaa

---
+++ build/config.js
@@ -0,0 +1,19 @@
+"use strict";
+module.exports = {
+    clientId: "1104416492025544705",
+    color: "#2f3136",
+    /*token: process.env.BOT_TOKEN,
+    dev: {
+        testGuild: "GuildCommandを使用するサーバー",
+        developer: [
+            "開発者のID(Array)"
+        ]
+    },
+    logch: {
+    ready: "起動ログチャンネル",
+    command: "コマンド実行ログチャンネル",
+    error: "エラーログ出力チャンネル",
+    guildCreate: "サーバー参加ログチャンネル",
+    guildDelete: "サーバー退出ログチャンネル"
+    }*/
+};
